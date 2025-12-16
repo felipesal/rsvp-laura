610156ff7f2af4a2b64f3b93fa76680c928fd7a7
Felipe Salles <prof.felipe.salles@gmail.com>
2025-12-16 20:11:38 -0300

    Ajustes emergenciais

--- app/api/admin/guests/route.ts
+++ app/api/admin/guests/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
     const confirmUrl = `${baseUrl}/confirm/${token}`;
 
     const message = `Eeei ${data.name}, é com grande alegria que estamos te convidando para a festa da Laurinha, no dia 19/12 às 19:30hhs. Clique no link e confirme a sua presença.
-    Gostaria de pedir que, se possível, confirme até o dia 15/12 pois precisamos fechar a lista de convidados. Laurinha tá ansiosa pela sua presença! Até lá!`;
+    Gostaria de pedir que, se possível, confirme até o dia 17/12 pois precisamos fechar a lista de convidados. Laurinha tá ansiosa pela sua presença! Até lá!`;
 
     console.log(message);
 
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
       body: JSON.stringify({
         phone: `55${data.phone}`,
         link: confirmUrl,
-        thumnail_url: "https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/08/Kpop-Demon-Hunters.png?w=1024",
+        thumbnail_url: "https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/08/Kpop-Demon-Hunters.png?w=1024",
         title: "Confirme sua presença no aniversario da Laura",
         message: message
       })
--- app/confirm/[token]/page.tsx
+++ app/confirm/[token]/page.tsx
@@ -1,8 +1,9 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { Card, CardContent } from "@/components/ui/card";
 import RecoveredInviteForm from "@/components/recovered-invite-form";
 import { getBaseUrl } from "@/lib/utils";
 import { Invite } from "@/types/types";
+import { Button } from "@/components/ui/button";
 
 async function getInvite(token: string) {
   const baseUrl = getBaseUrl();
@@ -22,11 +23,22 @@ export default async function ValidateInvitePage({ params }: { params: { token: 
   console.log(invite);
   if (!invite) return notFound();
 
+  function directToInvite() {
+    redirect("/about");
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center p-4 bg-[url('/background.png')] bg-cover bg-center bg-black/50 bg-blend-multiply">
       <Card className="w-full max-w-lg shadow-xl">
         <CardContent className="p-6 space-y-6">
           <h1 className="text-2xl font-bold text-center">Detalhes do Convite</h1>
+          <p>A festa ocorrerá no dia 19/12/2025 às 19:30hs.</p>
+          <Button
+          onClick={directToInvite}
+          className="w-full bg-pink-600 hover:bg-pink-700 text-white"
+        >
+          Mais Detalhes
+        </Button>
           <RecoveredInviteForm invite={invite as Invite} token={token} />
         </CardContent>
       </Card>
